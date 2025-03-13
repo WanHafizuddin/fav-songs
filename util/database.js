45cfdbca46ffb05d54d44893dbcca303636712a4
WanHafizuddin <whafiz012@gmail.com>
2025-03-13 23:43:04 +0800

    updated

--- util/database.js
+++ util/database.js
@@ -1,11 +1,13 @@
-require('dotenv').config();
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+require("dotenv").config(); // Load environment variables
 
 const pool = new Pool({
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT || 5432, // Default PostgreSQL port
+  user: process.env.PG_USER,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false, // Required for Render
+  },
 });
-
-module.exports = pool;
